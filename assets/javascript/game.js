3ed17845b95800541168e4413a2520a106783437
Courtney Duggins <Marie1990@wifi_stu-10-38-66-43.uncc.edu>
2018-07-11 18:19:51 -0400

    add pseudocode and define variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// define variable for user guess, computer's choice, wins, losses &
+// and number of guess left
+
+// set wins and loss equal to starting count, 0
+// set guesses Left equal to starting number, 9
+
+var userGuess = event.key;
+var computerChoice
+var wins = 0;
+var losses = 0;
+var guessesLeft  = 9;
+
+// when user presses key, determine if key pressed matches computer's choice
+
+
+
+// if user guess matches computer choice, increase wins
