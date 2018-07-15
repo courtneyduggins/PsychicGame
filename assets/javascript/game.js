c5dfd21b46386e38da4258044e16a67a9f66f9f5
Courtney Duggins <Marie1990@Courtneys-MacBook-Pro.local>
2018-07-14 21:42:34 -0400

    add random letter generator and fo-loops

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,29 +3,91 @@
 // set wins and loss equal to starting count, 0
 // set guessesRemaining equal to starting number, 9
 
-var userGuess = event.key;
-var winsCounter = 0;
-var lossesCounter = 0;
-var guessesLeft  = 9;
+// var userGuess = [], x = 9;
 
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"];
 
 
 // Computer selects random letter, store computerChoice...
-computerChoice = new Random();
-(char) (computerChoice.nextInt(26) + 'a');
 
 
 // Game beings when user presses key....
-var guesses = document.getElementById("yourGuesses");
+// var guesses = document.getElementById("yourGuesses");
 document.onkeyup = function (event) {
-guesses.append(userGuess);
+    var userGuess = event.key.toLowerCase();
+    var computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log(computerPick);
+
+    var guesses = [];
+        // guesses.push(userGuess);
+        
+    
+
+    for (var i = 0; i < 9; i++) {
+        
+        guesses.push(userGuess);
+        console.log(guesses);
+    }
+    
+
+    for (var j = 0; j<guesses.length; j++){
+        // System.out.print(guesses[i]);
+
+        if (userGuess === computerPick) {
+            wins++;
+        }
+        else {
+            losses++;
+            guessesLeft--;
+        }
+}
+
+        
+    
+
+    var html =
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses left: " + guessesLeft+ "</p>" +
+        "<p>Your guesses so far: " + guesses[j] + "</p>";
+        
+    
+    // Set the inner HTML contents of the #game div to our html string
+    document.querySelector("#game").innerHTML = html;
+
+
+}
+
+
+
+
+
+    // while (userGuess.length < x) {
+    //     userGuess.push(guesses);
+    // }
+    // console.log(userGuess);
+
+    // for (var i = 0; i < userGuess.length; i++) {
+
+    //     var html =
+    //       "<p>Your guesses so far: " + userGuess[i] + "</p>";
+    //   "<p>The computer chose: " + computerGuess + "</p>" +
+    //   "<p>wins: " + wins + "</p>" +
+    //   "<p>losses: " + losses + "</p>" +
+    //   "<p>ties: " + ties + "</p>";
+
+    // }
+
 
     // var userGuess1 = document.getElementById("yourGuesses");
     // userGuess1.appendChild(userGuess);
 
 
 
-}
+
 
 
 
